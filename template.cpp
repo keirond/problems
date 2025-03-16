d9c46de895b7ec60ab334fed14285822c5aee629
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-16 21:38:40 +0700

    up

--- template.cpp
+++ template.cpp
@@ -115,18 +115,17 @@ void _print(T t, V... v) {
 #define dbg(x...) 
 #endif
 
-
 // clang-format on
 
 void solve(int test_case [[maybe_unused]]) {}
 
 int main() {
-  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  int test_cases = 1;
-  cin >> test_cases;
-  while (test_cases--) {
-    solve(test_cases);
-    cout << flush;
-  }
-  return 0;
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve(test_cases);
+        cout << flush;
+    }
+    return 0;
 }
