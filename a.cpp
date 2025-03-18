0eaa6f8030e12c01e461655a60bab858d593ff8c
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-18 22:16:05 +0700

    up

--- a.cpp
+++ a.cpp
@@ -118,6 +118,8 @@ void solve(int test_case [[maybe_unused]]) {}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
+	info("Enabling debug logging...");;
+
 	int test_cases = 1;
 	cin >> test_cases;
 	while (test_cases--) {
