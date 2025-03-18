d6b3abcafba2733ba457358e38cb0823121c83d8
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-18 12:46:21 +0700

    up

--- cfs/52C1.cpp
+++ cfs/52C1.cpp
@@ -119,16 +119,18 @@ template <typename T, typename... V> void _print(T t, V... v) {
 
 struct STree {
 	int n;
-	vl tree, lazy;
+	vector<long long> tree, lazy;
+	vector<bool> pending;
 
 	STree(vector<int> &nums) {
 		n = nums.size();
 		tree.assign(n << 2, 0);
 		lazy.assign(n << 2, 0);
+		pending.assign(n << 2, 0);
 		build(nums, 1, 0, n - 1);
 	}
 
-	void build(vi &nums, int node, int l, int r) {
+	void build(vector<int> &nums, int node, int l, int r) {
 		if (l == r) {
 			tree[node] = nums[l];
 			return;
@@ -136,17 +138,19 @@ struct STree {
 		int m = l + (r - l >> 1);
 		build(nums, node << 1, l, m);
 		build(nums, node << 1 | 1, m + 1, r);
-		tree[node] = min(tree[node << 1], tree[node << 1 | 1]);
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
 	}
 
 	void push(int node, int l, int r) {
-		if (lazy[node]) {
-			tree[node] += lazy[node];
+		if (pending[node]) {
+			tree[node] = lazy[node] * (r - l + 1);
 			if (l != r) {
-				lazy[node << 1] += lazy[node];
-				lazy[node << 1 | 1] += lazy[node];
+				lazy[node << 1] = lazy[node];
+				lazy[node << 1 | 1] = lazy[node];
+				pending[node << 1] = pending[node << 1 | 1] = true;
 			}
 			lazy[node] = 0;
+			pending[node] = false;
 		}
 	}
 
@@ -154,28 +158,28 @@ struct STree {
 		push(node, l, r);
 		if (qr < l || ql > r) return;
 		if (ql <= l && r <= qr) {
-			lazy[node] += val;
+			lazy[node] = val;
 			push(node, l, r);
 			return;
 		}
 		int m = l + (r - l >> 1);
 		update(node << 1, l, m, ql, qr, val);
 		update(node << 1 | 1, m + 1, r, ql, qr, val);
-		tree[node] = min(tree[node << 1], tree[node << 1 | 1]);
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
 	}
 
-	ll query(int node, int l, int r, int ql, int qr) {
+	long long query(int node, int l, int r, int ql, int qr) {
 		push(node, l, r);
-		if (qr < l || ql > r) return INT_MAX;
+		if (qr < l || ql > r) return 0;
 		if (ql <= l && r <= qr) return tree[node];
 		int m = l + (r - l >> 1);
-		return min(query(node << 1, l, m, ql, qr),
-				   query(node << 1 | 1, m + 1, r, ql, qr));
+		return query(node << 1, l, m, ql, qr) +
+			   query(node << 1 | 1, m + 1, r, ql, qr);
 	}
 
 	void update(int l, int r, int val) { update(1, 0, n - 1, l, r, val); }
 
-	ll query(int l, int r) { return query(1, 0, n - 1, l, r); }
+	long long query(int l, int r) { return query(1, 0, n - 1, l, r); }
 };
 
 void solve(int test_case [[maybe_unused]]) {
