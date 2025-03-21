d8441b63116da2b7b3967115b7069d7b736121c8
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-21 08:02:16 +0700

    up

--- cfs/done/961E.cpp
+++ cfs/done/961E.cpp
@@ -63,7 +63,56 @@ template <typename T, typename... V> void __print(T t, V... v) {
 
 // **************************************************************************
 
-void solve(int test_case [[maybe_unused]]) {}
+struct FTree {
+	int n;
+	vector<int> ft;
+
+	FTree(int n) : n(n), ft(n + 1) {}
+
+	void update(int i, int val) {
+		for (; i <= n; i += i & -i) ft.at(i) += val;
+	}
+
+	int query(int i) {
+		int ans = 0;
+		for (; i > 0; i -= i & -i) ans += ft.at(i);
+		return ans;
+	}
+
+	int query(int l, int r) { return query(r) - query(l - 1); }
+};
+
+void solve(int test_case [[maybe_unused]]) {
+	int N;
+	cin >> N;
+	vector<int> nums;
+	vector<pair<int, int>> t;
+	for (int i = 0; i < N; ++i) {
+		int d;
+		cin >> d;
+		nums.push_back(d);
+		t.push_back({d, i + 1});
+	}
+
+	sort(t.begin(), t.end());
+
+	info(nums, t);
+	FTree ft(N);
+
+	long long ans = 0;
+	int j = N - 1;
+	for (int i = N; i >= 1; --i) {
+		while (j >= 0 && t[j].first >= i) {
+			ft.update(t[j].second, 1);
+			j--;
+		}
+		info(ft.ft);
+		// a[ho] >= i
+		// a[i] >= ho (count ho)
+		if (nums[i - 1] >= i) ans += ft.query(i + 1, min(N, nums[i - 1]));
+	}
+	cout << ans << '\n';
+}
 
 // **************************************************************************
 
