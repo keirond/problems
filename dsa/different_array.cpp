30138833ce97f14029d7441b39e8a5f54dbad4ce
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-24 12:29:44 +0700

    up

---
+++ dsa/different_array.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct DiffArr {
+	int n;
+	vector<long long> diff, nums;
+
+	DiffArr(vector<int> &nums) {
+		n = nums.size();
+		diff.assign(n + 1, 0);
+		this->nums.assign(nums.begin(), nums.end());
+
+		diff[0] = nums[0];
+		for (int i = 1; i < n; ++i) {
+			diff[i] = nums[i] - nums[i - 1];
+		}
+	}
+
+	void reconstruct() {
+		nums[0] = diff[0];
+		for (int i = 1; i < n; ++i) {
+			nums[i] = nums[i - 1] + diff[i];
+		}
+	}
+
+	void update(int l, int r, int val) {
+		diff[l] += val;
+		diff[r + 1] -= val;
+	}
+
+	long long query(int i) {
+		long long ans = 0;
+		for (int j = 0; j <= i; ++j) {
+			ans += diff[j];
+		}
+		return ans;
+	}
+
+	long long query_after_reconstruct(int i) { return nums[i]; }
+};
+
+int main() { return 1; }
