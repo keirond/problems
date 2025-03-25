79a262d220177824e9348b70928dd1224687c97f
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-25 23:55:12 +0700

    up

--- dsa/trie/persistent_trie.cpp
+++ dsa/trie/persistent_trie.cpp
@@ -2,16 +2,61 @@
 
 using namespace std;
 
-struct Trie {
-	vector<Trie*> link;
+struct Node {
+	int next[26];
+	bool end;
 
-	Trie() {}
-	
-	void insert(string &word, int len) {
-		if(len == key.length()) {
-			return new Trie(
+	Node() {
+		for (int i = 0; i < 26; i++) {
+			next[i] = 0;
 		}
-	}	
+		end = false;
+	}
+};
+
+struct Trie {
+	int cnt;
+	vector<Node> trie;
+	vector<int> root;
+
+	Trie() : cnt(0) { root.push_back(new_node()); }
+
+	int new_node() {
+		if (cnt >= trie.size())
+			trie.push_back(Node());
+		else
+			trie[cnt] = Node();
+		return cnt++;
+	}
+
+	void insert(string &word) { root.push_back(insert(root.back(), word)); }
+
+	int insert(int prev, string &word) {
+		int cur = new_node();
+		int node = cur;
+		int old = prev;
+		for (char c : word) {
+			int idx = c - 'a';
+			if (old) {
+				trie[cur] = trie[old];
+				old = trie[old].next[idx];
+			}
+			trie[cur].next[idx] = new_node();
+			cur = trie[cur].next[idx];
+		}
+		trie[cur].end = true;
+		return node;
+	}
+
+	bool search(string &word) {
+		int cur = root.back();
+		for (char c : word) {
+			int idx = c - 'a';
+			if (!trie[cur].next[idx]) return false;
+			cur = trie[cur].next[idx];
+		}
+		return trie[cur].end;
+	}
 };
 
 int main() { return 0; }
