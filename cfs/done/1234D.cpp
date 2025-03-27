ee031cc9908bf98c8c0f00a87fb2661d12866179
Bao. Dang <dang.hbaokody@gmail.com>
2025-03-28 01:05:39 +0700

    up

--- cfs/done/1234D.cpp
+++ cfs/done/1234D.cpp
@@ -63,45 +63,60 @@ template <typename T, typename... V> void __print(T t, V... v) {
 
 // **************************************************************************
 
-struct FTree {
+struct STree {
 	int n;
-	vector<int> ft[26];
-	string str;
+	vector<int> tree;
 
-	FTree(string str) {
-		this->n = str.size();
-		for (int i = 0; i < 26; ++i) this->ft[i].assign(n + 1, 0);
-		this->str = str;
-		for (int i = 0; i < n; ++i) build(i + 1, str[i] - 'a');
+	STree(string &s) {
+		n = s.size();
+		tree.assign(n << 2, 0);
+		build(s, 1, 0, n - 1);
 	}
 
-	void build(int i, int ic) {
-		for (; i <= n; i += i & -i) ft[ic][i]++;
-	}
-
-	void assign(int i, char c) {
-		update(i, str[i - 1] - 'a', c - 'a');
-		str[i - 1] = c;
-	}
-
-	void update(int i, int oc, int nc) {
-		for (; i <= n; i += i & -i) {
-			ft[oc][i]--;
-			ft[nc][i]++;
+	void build(string &s, int node, int l, int r) {
+		if (l == r) {
+			int idx = s[l] - 'a';
+			tree[node] = 1 << idx;
+			return;
 		}
+		int m = l + (r - l >> 1);
+		build(s, node << 1, l, m);
+		build(s, node << 1 | 1, m + 1, r);
+		tree[node] = tree[node << 1] | tree[node << 1 | 1];
 	}
 
-	int _query(int i, int ic) {
-		int ans = 0;
-		for (; i > 0; i -= i & -i) ans += ft[ic][i];
-		return ans;
+	void update(int node, int l, int r, int i, int val) {
+		if (l == r) {
+			tree[node] = val;
+			return;
+		}
+		int m = l + (r - l >> 1);
+		if (i <= m)
+			update(node << 1, l, m, i, val);
+		else
+			update(node << 1 | 1, m + 1, r, i, val);
+		tree[node] = tree[node << 1] | tree[node << 1 | 1];
+	}
+
+	void update(int i, char c) {
+		int idx = c - 'a';
+		update(1, 0, n - 1, i, 1 << idx);
+	}
+
+	int query(int node, int l, int r, int ql, int qr) {
+		if (ql > r || qr < l) return 0;
+		if (ql <= l && r <= qr) return tree[node];
+		int m = l + (r - l >> 1);
+		return query(node << 1, l, m, ql, qr) |
+			   query(node << 1 | 1, m + 1, r, ql, qr);
 	}
 
 	int query(int l, int r) {
+		int t = query(1, 0, n - 1, l, r);
 		int ans = 0;
-		for (int i = 0; i < 26; ++i) {
-			int t = _query(r, i) - _query(l - 1, i);
-			ans += t > 0 ? 1 : 0;
+		while (t) {
+			ans++;
+			t &= t - 1;
 		}
 		return ans;
 	}
@@ -111,7 +126,7 @@ void solve(int test_case [[maybe_unused]]) {
 	string S;
 	cin >> S;
 
-	FTree ft(S);
+	STree st(S);
 
 	int N;
 	cin >> N;
@@ -121,10 +136,10 @@ void solve(int test_case [[maybe_unused]]) {
 		cin >> op;
 		if (op == 1) {
 			cin >> l >> c;
-			ft.assign(l, c);
+			st.update(l - 1, c);
 		} else if (op == 2) {
 			cin >> l >> r;
-			cout << ft.query(l, r) << '\n';
+			cout << st.query(l - 1, r - 1) << '\n';
 		}
 	}
 }
