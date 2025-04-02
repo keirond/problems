bcb6c2c6ee9dc6fbf0d1e564c8c5e0292bed80d6
Bao. Dang <dang.hbaokody@gmail.com>
2025-04-03 00:19:36 +0700

    up

--- dsa/number_theory/sieve.cpp
+++ dsa/number_theory/sieve.cpp
@@ -3,71 +3,63 @@
 using namespace std;
 
 using ll = long long;
+constexpr char nl = '\n';
 
-void sieve(ll n) {
-	vector<ll> ans(n + 1, 1);
-	ans[0] = ans[1] = 0;
-	for (ll i = 2; i * i <= n; ++i) {
-		if (ans[i]) {
-			for (ll j = i * i; j <= n; j += i) ans[j] = 0;
-		}
+void sieve(int n) {	 // up to 1e7
+	vector<bool> isp(n + 1, 1);
+	for (int i = 2; i * i <= n; ++i) {
+		if (isp[i])
+			for (ll j = i * i; j <= n; j += i) isp[j] = 0;
 	}
-	for (ll i = 0; i <= n; ++i)
-		if (ans[i]) cout << i << ' ';
-	cout << '\n';
+	for (int i = 2; i <= n; ++i)
+		if (isp[i]) cout << i << ' ';
+	cout << nl;
 }
 
-void bsieve(ll n) {
-	vector<ll> ans((n >> 5) + 1);
-	ans[0] |= 3;
+void bsieve(int n) {  // up to 1e7
+	vector<int> ans((n >> 5) + 1);
 	for (ll i = 2; i * i <= n; ++i) {
-		if (!(ans[i >> 5] & (1 << (i & 31)))) {
+		if (!(ans[i >> 5] & (1 << (i & 31))))
 			for (ll j = i * i; j <= n; j += i) ans[j >> 5] |= 1 << (j & 31);
-		}
 	}
-	for (ll i = 0; i <= n; ++i)
+	for (ll i = 2; i <= n; ++i)
 		if (!(ans[i >> 5] & (1 << (i & 31)))) cout << i << ' ';
-	cout << '\n';
+	cout << nl;
 }
 
-void bsieve_bitset(ll n) {
-	bitset<10000001> ans;
-	ans.set();
-	ans[0] = ans[1] = 0;
-}
-
-void ssieve(ll l, ll r) {
-	vector<ll> primes = {};	 // sieve((ll)sqrt(r));
-	vector<bool> is_primes(r - l + 1, 1);
-	for (auto p : primes) {
-		ll start = max(p * p, (l + p - 1) / p * p);
-		for (ll j = start; j <= r; j += p) is_primes[j - l] = 0;
-	}
-
-	if (l == 1) is_primes[0] = 0;
-	for (ll i = l; i <= r; ++i)
-		if (is_primes[i - l]) cout << i << ' ';
-	cout << '\n';
-}
-
-void esieve(ll n) {
-	vector<ll> primes;
-	vector<bool> is_primes(n + 1, 1);
-	is_primes[0] = is_primes[1] = 0;
-	for (ll i = 2; i * i <= n; ++i) {
-		if (is_primes[i]) primes.push_back(i);
-		for (auto p : primes) {
-			ll x = i * p;
-			if (x > n) break;
-			is_primes[x] = 0;
+void esieve(int n) {  // up to 1e8
+	vector<int> ps;
+	vector<bool> isp(n + 1, 1);
+	for (int i = 2; i <= n; ++i) {
+		if (isp[i]) ps.push_back(i);
+		for (int p : ps) {
+			int t = i * p;
+			if (t > n) break;
+			isp[t] = 0;
 			if (i % p == 0) break;
 		}
 	}
+	for (int p : ps) cout << p << ' ';
+	cout << nl;
+}
+
+void ssieve(ll l, ll r) {
+	vector<ll> ps;
+	// standard sieve for sqrt(n)
+	vector<bool> isp(r - l + 1, 1);
+	for (ll p : ps) {
+		ll start = max(p * p, (l + p - 1) / p * p);
+		for (ll j = start; j <= r; j += p) isp[j - l] = 0;
+	}
+	if (l == 1) isp[0] = 0;
+	for (ll i = l; i <= r; ++i)
+		if (isp[i - l]) cout << i << ' ';
+	cout << nl;
 }
 
 int main() {
-	ll n = 234;
-	sieve(n);
-	bsieve(n);
+	sieve(1e7);
+	bsieve(1e7);
+	esieve(1e8);
 	return 0;
 }
