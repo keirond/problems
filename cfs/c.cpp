bda35e89a90e4a6c29a456384174fcb812e024a8
Bao. Dang <dang.hbaokody@gmail.com>
2025-04-05 22:31:31 +0700

    up

--- cfs/c.cpp
+++ cfs/c.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-using namespace std;
-
 using ll = long long;
 
 #define fi first
@@ -18,58 +16,73 @@ using ll = long long;
 
 constexpr char nl [[maybe_unused]] = '\n';
 
+// --------------------------------------------------------------------------
+
+template <typename T> void __info(const T &x);
+template <typename T, typename V> void __info(const pair<T, V> &x);
+template <typename T, typename V, typename K>
+void __info(const tuple<T, V, K> &x);
+
+void __info(int x) { cerr << x; }
+void __info(long x) { cerr << x; }
+void __info(long long x) { cerr << x; }
+void __info(unsigned x) { cerr << x; }
+void __info(unsigned long x) { cerr << x; }
+void __info(unsigned long long x) { cerr << x; }
+void __info(float x) { cerr << x; }
+void __info(double x) { cerr << x; }
+void __info(long double x) { cerr << x; }
+void __info(char x) { cerr << '\'' << x << '\''; }
+void __info(const char *x) { cerr << '\"' << x << '\"'; }
+void __info(const string &x) { cerr << '\"' << x << '\"'; }
+void __info(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T> void __info(const T &x) {
+	int f = 0;
+	cerr << '{';
+	for (auto &i : x) cerr << (f++ ? ", " : ""), __info(i);
+	cerr << "}";
+}
+
+template <typename T, typename V> void __info(const pair<T, V> &x) {
+	cerr << '{', __info(x.first), cerr << ", ", __info(x.second), cerr << '}';
+}
+
+template <typename T, typename V, typename K>
+void __info(const tuple<T, K, V> &x) {
+	cerr << '{', __info(get<0>(x)), cerr << ", ", __info(get<1>(x)),
+		cerr << ", ", __info(get<2>(x)), cerr << '}';
+}
+
+void __print() { cerr << ']' << nl; }
+
+template <typename T, typename... V> void __print(T t, V... v) {
+	__info(t);
+	if (sizeof...(v)) cerr << ", ";
+	__print(v...);
+}
+
+#ifdef INFO
+#define enable_info() cerr << "Enabling debug logging..." << nl;
+#define info(x...)                                                            \
+	cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; \
+	__print(x);                                                               \
+	cerr << "\e[39m";
+#else
+#define enable_info()
+#define info(x...)
+#endif
+
 // **************************************************************************
 
-const int MAXV = 1e8 + 1;
-vector<int> ps;
-
-void sieve() {
-	vector<bool> isp(MAXV, 1);
-
-	for (ll i = 2; i * i < MAXV; i++) {
-		if (isp[i]) {
-			for (ll j = i * i; j < MAXV; j += i) isp[j] = 0;
-		}
-	}
-	for (int i = 2; i < MAXV; i++) {
-		if (isp[i]) ps.pb(i);
-	}
-}
-
-void solve(int test_case [[maybe_unused]]) {
-	int N;
-	cin >> N;
-
-	sieve();
-	cerr << ps.size() << nl;
-	while (N--) {
-		ll X;
-		cin >> X;
-		ll t = X;
-
-		ll ans = 1;
-		for (int i = 0; i < ps.size() && ps[i] * ps[i] <= t; i++) {
-			ll p = ps[i];
-			if (t % p == 0) {
-				ll cnt = 0;
-				while (t % p == 0) {
-					t /= p;
-					cnt++;
-				}
-				ans *= (pow(p, cnt + 1) - 1) / (p - 1);
-			}
-		}
-
-		if (t != 1) ans *= t + 1;
-		cout << ans - X << nl;
-	}
-}
+void solve(int test_case [[maybe_unused]]) {}
 
 // **************************************************************************
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
+	enable_info();
 
 	int test_cases = 1;
 	// cin >> test_cases;
