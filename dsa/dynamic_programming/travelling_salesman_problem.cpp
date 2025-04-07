b8e4da86ba22d9a70b0242f44a6227fa86bd9c5d
Bao. Dang <dang.hbaokody@gmail.com>
2025-04-07 12:02:43 +0700

    up

---
+++ dsa/dynamic_programming/travelling_salesman_problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+#define fi first
+#define se second
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define up upper_bound
+#define sz(v) (int)(v).size()
+#define all(v) v.begin(), v.end()
+#define range(v, n) v.begin(), v.begin() + n
+
+constexpr char nl [[maybe_unused]] = '\n';
+
+// **************************************************************************
+
+void solve(int test_case [[maybe_unused]]) {
+	int C;
+	cin >> C;
+	vector<vector<int>> distances;
+	while (C--) {
+		int u, v, d;
+		cin >> u >> v >> d;
+		distances.pb({u, v, d});
+	}
+}
+
+// **************************************************************************
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	return 0;
+}
+
+// **************************************************************************
+// *author* Keiron Dang
+// **************************************************************************
