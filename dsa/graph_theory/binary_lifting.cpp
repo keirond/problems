8a1cf31e86bfc8e5c1151cbad7cbc00014953dc2
Bao. Dang <dang.hbaokody@gmail.com>
2025-04-09 17:15:18 +0700

    up

--- dsa/graph_theory/binary_lifting.cpp
+++ dsa/graph_theory/binary_lifting.cpp
@@ -18,29 +18,65 @@ constexpr char nl [[maybe_unused]] = '\n';
 
 // **************************************************************************
 
+int LOG = 20;
+
 vector<vector<int>> adj;
+vector<vector<int>> dp;
 vector<int> depth;
 
-void dfs(int i) {
-
-}
-
-int lca(int u, int v) {}
-
-void solve(int test_case [[maybe_unused]]) {
-	int N, P;
-	cin >> N >> P;
-	adj.resize(N);
-	depth.resize(N);
-	while (P--) {
-		int u, v;
-		cin >> u >> v;
-		adj[u].pb(v);
-		adj[v].pb(u);
+void dfs(int u, int p) {
+	dp[u][0] = p;
+	for (int i = 1; i < LOG; i++) {
+		dp[u][i] = dp[dp[u][i - 1]][i - 1];
 	}
 
-	int Q;
-	cin >> Q;
+	for (int v : adj[u]) {
+		if (v != p) {
+			depth[v] = depth[u] + 1;
+			dfs(v, u);
+		}
+	}
+}
+
+int lift(int u, int k) {
+	for (int i = 0; i < LOG; i++) {
+		if (k & (1 << i)) {
+			u = dp[u][i];
+		}
+	}
+	return u;
+}
+
+int lca(int u, int v) {
+	if (depth[u] < depth[v]) swap(u, v);
+	u = lift(u, depth[u] - depth[v]);
+
+	if (u == v) return u;
+	for (int i = LOG - 1; i >= 0; i--) {
+		if (dp[u][i] != dp[v][i]) {
+			u = dp[u][i];
+			v = dp[v][i];
+		}
+	}
+	return dp[u][0];
+}
+
+void solve(int test_case [[maybe_unused]]) {
+	int N, Q;
+	cin >> N >> Q;
+	LOG = log(N * 2 - 1) / log(2);
+
+	adj.resize(N);
+	depth.resize(N);
+	for (int i = 1; i < N; i++) {
+		int p;
+		cin >> p;
+		adj[p].pb(i);
+	}
+
+	dp.assign(N, vector<int>(LOG));
+	dfs(0, 0);
+
 	while (Q--) {
 		int u, v;
 		cin >> u >> v;
