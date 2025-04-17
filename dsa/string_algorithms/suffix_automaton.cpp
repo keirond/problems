4c86967367181a2cea3bb4a82618e559e62434c0
Bao. Dang <dang.hbaokody@gmail.com>
2025-04-18 02:02:31 +0700

    up

--- dsa/string_algorithms/suffix_automaton.cpp
+++ dsa/string_algorithms/suffix_automaton.cpp
@@ -21,7 +21,7 @@ constexpr char nl [[maybe_unused]] = '\n';
 const int A = 26;
 struct SA {
 	int last = 0, sz = 0;
-	vector<int> link, len;
+	vector<int> link, len, count;
 	vector<vector<int>> next;
 
 	SA() { add_node(); }
@@ -30,6 +30,7 @@ struct SA {
 		len.emplace_back();
 		next.emplace_back(A, -1);
 		link.emplace_back(-1);
+		count.emplace_back();
 		return sz++;
 	}
 
@@ -37,6 +38,7 @@ struct SA {
 		int u = add_node();
 		int d = c - 'a', p = last;
 		len[u] = len[p] + 1;
+		count[u] = 1;
 
 		while (p != -1 && next[p][d] == -1) {
 			next[p][d] = u;
@@ -63,19 +65,78 @@ struct SA {
 		}
 		last = u;
 	}
+
+	bool contains(const string &ss) {
+		int u = 0;
+		for (char c : ss) {
+			int d = c - 'a';
+			if (next[u][d] == -1) return false;
+			u = next[u][d];
+		}
+		return true;
+	}
+
+	ll distinct_substring() {
+		ll ans = 0;
+		for (int i = 1; i < sz; i++) {
+			ans += len[i] - len[link[i]];
+		}
+		return ans;
+	}
+
+	void count_occurrence() {
+		vector<int> order(sz);
+		iota(all(order), 0);
+		sort(all(order), [&](int a, int b) { return len[a] > len[b]; });
+
+		for (int d : order) {
+			if (link[d] != -1) {
+				count[link[d]] += count[d];
+			}
+		}
+	}
+
+	int longest_common_substring(const string &b) {
+		int u = 0, l = 0, ans = 0;
+		for (char c : b) {
+			int d = c - 'a';
+			if (next[u][d] != -1) {
+				l++;
+				u = next[u][d];
+			} else {
+				while (u != -1 && next[u][d] == -1) {
+					u = link[u];
+				}
+				if (u == -1) {
+					l = 0;
+					u = 0;
+				} else {
+					l = len[u] + 1;
+					u = next[u][d];
+				}
+			}
+			ans = max(ans, l);
+		}
+		return ans;
+	}
 };
 
 void solve(int test_case [[maybe_unused]]) {
-	string s;
-	cin >> s;
+	string s = "abracadabra";
+	cout << s << nl;
+
 	SA sa;
 	for (char c : s) sa.extend(c);
 
-	ll ans = 0;
-	for (int i = 1; i < sa.sz; i++) {
-		ans += sa.len[i] - sa.len[sa.link[i]];
+	cout << sa.contains("cada") << nl;
+	cout << sa.contains("abc") << nl;
+	cout << sa.distinct_substring() << nl;
+	sa.count_occurrence();
+	for (int d : sa.count) {
+		cout << d << ' ';
 	}
-	cout << ans << nl;
+	cout << nl;
+	cout << sa.longest_common_substring("bbracxdabradascv") << nl;
 }
 
 // **************************************************************************
