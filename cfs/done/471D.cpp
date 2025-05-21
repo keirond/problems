42f3545ff539a62e07f70465eb3a03fa62b80ed9
Bao Dang <dang.hbaokody@gmail.com>
2025-05-21 14:00:23 +0700

    up Wed May 21 07:00:23 AM UTC 2025

---
+++ cfs/done/471D.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+#define fi first
+#define se second
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(v) (int)(v).size()
+#define all(v) v.begin(), v.end()
+#define range(v, n) v.begin(), v.begin() + n
+
+constexpr char nl [[maybe_unused]] = '\n';
+
+// --------------------------------------------------------------------------
+
+template <typename T> void __info(const T &x);
+template <typename T, typename V> void __info(const pair<T, V> &x);
+template <typename T, typename V, typename K>
+void __info(const tuple<T, V, K> &x);
+
+void __info(int x) { cerr << x; }
+void __info(long x) { cerr << x; }
+void __info(long long x) { cerr << x; }
+void __info(unsigned x) { cerr << x; }
+void __info(unsigned long x) { cerr << x; }
+void __info(unsigned long long x) { cerr << x; }
+void __info(float x) { cerr << x; }
+void __info(double x) { cerr << x; }
+void __info(long double x) { cerr << x; }
+void __info(char x) { cerr << '\'' << x << '\''; }
+void __info(const char *x) { cerr << '\"' << x << '\"'; }
+void __info(const string &x) { cerr << '\"' << x << '\"'; }
+void __info(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T> void __info(const T &x) {
+	int f = 0;
+	cerr << '{';
+	for (auto &i : x) cerr << (f++ ? ", " : ""), __info(i);
+	cerr << "}";
+}
+
+template <typename T, typename V> void __info(const pair<T, V> &x) {
+	cerr << '{', __info(x.first), cerr << ", ", __info(x.second), cerr << '}';
+}
+
+template <typename T, typename V, typename K>
+void __info(const tuple<T, K, V> &x) {
+	cerr << '{', __info(get<0>(x)), cerr << ", ", __info(get<1>(x)),
+		cerr << ", ", __info(get<2>(x)), cerr << '}';
+}
+
+void __print() { cerr << ']' << nl; }
+
+template <typename T, typename... V> void __print(T t, V... v) {
+	__info(t);
+	if (sizeof...(v)) cerr << ", ";
+	__print(v...);
+}
+
+#ifdef INFO
+#define enable_info() cerr << "Enabling debug logging..." << nl;
+#define info(x...)                                                            \
+	cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; \
+	__print(x);                                                               \
+	cerr << "\e[39m";
+#else
+#define enable_info()
+#define info(x...)
+#endif
+
+// **************************************************************************
+
+vector<int> compute(vector<int> &nums) {
+	int n = nums.size();
+	vector<int> pi(n);
+	for (int i = 1, j = 0; i < n; i++) {
+		while (j > 0 && nums[i] != nums[j]) j = pi[j - 1];
+		if (nums[i] == nums[j]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+void solve(int test_case [[maybe_unused]]) {
+	int N, M;
+	cin >> N >> M;
+	vector<int> bears(N), walls(M);
+	for (int &d : bears) cin >> d;
+	for (int &d : walls) cin >> d;
+
+	if (M == 1) {
+		cout << N << nl;
+		return;
+	}
+
+	vector<int> nbears(N - 1), nwalls(M - 1);
+	for (int i = 0; i < N - 1; i++) {
+		nbears[i] = bears[i + 1] - bears[i];
+	}
+	for (int i = 0; i < M - 1; i++) {
+		nwalls[i] = walls[i + 1] - walls[i];
+	}
+
+	int ans = 0;
+	vector<int> pi = compute(nwalls);
+	for (int i = 0, j = 0; i < N - 1; i++) {
+		while (j > 0 && nbears[i] != nwalls[j]) j = pi[j - 1];
+		if (nbears[i] == nwalls[j]) j++;
+		if (j == M - 1) {
+			ans++;
+			j = pi[j - 1];
+		}
+	}
+	cout << ans << nl;
+}
+
+// **************************************************************************
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	enable_info();
+
+	int test_cases = 1;
+	// cin >> test_cases;
+	while (test_cases--) {
+		solve(test_cases);
+		cout << flush;
+	}
+	return 0;
+}
+
+// **************************************************************************
+// *author* Keiron Dang
+// **************************************************************************
