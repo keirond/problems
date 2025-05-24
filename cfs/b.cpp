18e2b052c73ca5ca080fa1014e4e2f0af397ea5a
Bao Dang <dang.hbaokody@gmail.com>
2025-05-24 19:35:22 +0700

    up Sat May 24 12:35:22 PM UTC 2025

--- cfs/b.cpp
+++ cfs/b.cpp
@@ -4,97 +4,7 @@ using namespace std;
 
 using ll = long long;
 
-#define fi first
-#define se second
-#define pb push_back
-#define eb emplace_back
-#define ins insert
-#define mp make_pair
-#define mtp make_tuple
-#define lb lower_bound
-#define ub upper_bound
-
-#define sz(v) (int)(v).size()
-#define all(v) v.begin(), v.end()
-#define par(v, n) v.begin(), v.begin() + n
-
-#define range(i, s, e, j) \
-	for (auto i = (s); ((j) > 0) ? (i <= (e)) : (i >= (e)); i += (j))
-#define iter(i, n) range(i, 0, n - 1, 1)
-#define each(i, arr) for (auto &i : (arr))
-
-constexpr double EPS [[maybe_unused]] = 1e-9;
-constexpr double PI [[maybe_unused]] = 3.14159265358979323846;
 constexpr char nl [[maybe_unused]] = '\n';
-
-// --------------------------------------------------------------------------
-
-template <class T> using max_heap = priority_queue<T>;
-template <class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
-
-string to_upper(string a) {
-	return transform(a.begin(), a.end(), a.begin(), ::toupper), a;
-}
-string to_lower(string a) {
-	return transform(a.begin(), a.end(), a.begin(), ::tolower), a;
-}
-
-// --------------------------------------------------------------------------
-
-template <typename T> void __info(const T &x);
-template <typename T, typename V> void __info(const pair<T, V> &x);
-template <typename T, typename V, typename K>
-void __info(const tuple<T, V, K> &x);
-
-void __info(int x) { cerr << x; }
-void __info(long x) { cerr << x; }
-void __info(long long x) { cerr << x; }
-void __info(unsigned x) { cerr << x; }
-void __info(unsigned long x) { cerr << x; }
-void __info(unsigned long long x) { cerr << x; }
-void __info(float x) { cerr << x; }
-void __info(double x) { cerr << x; }
-void __info(long double x) { cerr << x; }
-void __info(char x) { cerr << '\'' << x << '\''; }
-void __info(const char *x) { cerr << '\"' << x << '\"'; }
-void __info(const string &x) { cerr << '\"' << x << '\"'; }
-void __info(bool x) { cerr << (x ? "true" : "false"); }
-
-template <typename T> void __info(const T &x) {
-	int f = 0;
-	cerr << '{';
-	for (auto &i : x) cerr << (f++ ? ", " : ""), __info(i);
-	cerr << "}";
-}
-
-template <typename T, typename V> void __info(const pair<T, V> &x) {
-	cerr << '{', __info(x.first), cerr << ", ", __info(x.second), cerr << '}';
-}
-
-template <typename T, typename V, typename K>
-void __info(const tuple<T, K, V> &x) {
-	cerr << '{', __info(get<0>(x)), cerr << ", ", __info(get<1>(x)),
-		cerr << ", ", __info(get<2>(x)), cerr << '}';
-}
-
-void __print() { cerr << ']' << nl; }
-
-template <typename T, typename... V> void __print(T t, V... v) {
-	__info(t);
-	if (sizeof...(v)) cerr << ", ";
-	__print(v...);
-}
-
-#ifdef INFO
-#define enable_info() cerr << "Enabling debug logging..." << nl;
-#define info(x...)                                                            \
-	cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; \
-	__print(x);                                                               \
-	cerr << "\e[39m";
-#else
-#define enable_info()
-#define info(x...)
-#endif
 
 // **************************************************************************
 
@@ -105,7 +15,6 @@ void solve(int test_case [[maybe_unused]]) {}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
-	enable_info();
 
 	int test_cases = 1;
 	// cin >> test_cases;
