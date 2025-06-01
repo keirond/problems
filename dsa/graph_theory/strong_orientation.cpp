63cee180e2137e0ecb66f950fb2886493b7af348
Bao Dang <dang.hbaokody@gmail.com>
2025-06-02 03:05:17 +0700

    up Sun Jun  1 08:05:17 PM UTC 2025

--- dsa/graph_theory/strong_orientation.cpp
+++ dsa/graph_theory/strong_orientation.cpp
@@ -20,17 +20,62 @@ constexpr char nl [[maybe_unused]] = '\n';
 
 void solve(int test_case [[maybe_unused]]) {
 	int N, M;
-	cin >> N; // the number of vertices
-	cin >> M; // the number of edges
+	cin >> N >> M;
 
-	vector<pair<int, int>> edges;
-	vector<vector<pair<int, int>>> adj;
-	for(int i=0; i<M; i++) {
-		// next m lines describing the edges.
-		int U, V; cin >> U >> V;
+	vector<vector<int>> adj(N);
+	for (int i = 0; i < M; i++) {
+		int U, V;
+		cin >> U >> V;
 		U--, V--;
-		edges.push_back({U, V});
-		adj[U].push_back()
+		adj[U].push_back(V);
+		adj[V].push_back(U);
+	}
+
+	int idx = 0;
+	vector<int> disc(N, -1), lowlink(N, -1);
+	int bridge = 0;
+
+	function<void(int, int)> tarjan = [&](int u, int p) {
+		disc[u] = lowlink[u] = idx++;
+
+		for (int v : adj[u]) {
+			if (disc[v] == -1) {
+				tarjan(v, u);
+				lowlink[u] = min(lowlink[u], lowlink[v]);
+				if (disc[u] < lowlink[v]) {
+					bridge++;
+				}
+			} else if (v != p) {
+				lowlink[u] = min(lowlink[u], disc[v]);
+			}
+		}
+	};
+
+	for (int i = 0; i < N; i++) {
+		if (disc[i] == -1) tarjan(i, -1);
+	}
+
+	if (bridge)
+		cout << 0 << nl;
+	else {
+		vector<unordered_set<int>> ans(N);
+		vector<bool> vt(N);
+		function<void(int)> dfs = [&](int u) {
+			vt[u] = 1;
+			for (int v : adj[u]) {
+				if (!vt[v]) {
+					ans[u].insert(v);
+					cout << u + 1 << ' ' << v + 1 << nl;
+					dfs(v);
+				} else if (!ans[v].contains(u)) {
+					ans[u].insert(v);
+					cout << u + 1 << ' ' << v + 1 << nl;
+				}
+			}
+		};
+		for (int i = 0; i < N; i++) {
+			if (!vt[i]) dfs(i);
+		}
 	}
 }
 
