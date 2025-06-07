55c73fb0780946a8772f9aca54a44a9513b86641
Bao Dang <dang.hbaokody@gmail.com>
2025-06-07 20:49:18 +0700

    up Sat Jun  7 01:49:18 PM UTC 2025

---
+++ dsa/graph_theory/dijkstra.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+constexpr char nl [[maybe_unused]] = '\n';
+
+// **************************************************************************
+
+void solve(int test_case [[maybe_unused]]) {
+	int N, M; cin >> N, M;
+	vector<vector<int>
+	for(int )
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+}
+
+// **************************************************************************
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	solve(1);
+	cout << flush;
+	return 0;
+}
+
+// **************************************************************************
+// *author* Keiron Dang
+// **************************************************************************
