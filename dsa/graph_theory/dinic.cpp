4220de396daedf7bd298af2c1b600e5a40bc9cb8
Bao Dang <dang.hbaokody@gmail.com>
2025-06-08 21:01:35 +0700

    up Sun Jun  8 02:01:35 PM UTC 2025

---
+++ dsa/graph_theory/dinic.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+constexpr char nl [[maybe_unused]] = '\n';
+
+// **************************************************************************
+
+void solve(int test_case [[maybe_unused]]) {}
+
+// **************************************************************************
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	solve(1);
+	cout << flush;
+	return 0;
+}
+
+// **************************************************************************
+// *author* Keiron Dang
+// **************************************************************************
