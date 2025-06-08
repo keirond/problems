d306878de2ecc4e3f8ef36c72f465c09ab3b2d09
Bao Dang <dang.hbaokody@gmail.com>
2025-06-09 00:43:15 +0700

    up Sun Jun  8 05:43:15 PM UTC 2025

--- dsa/graph_theory/dinic.cpp
+++ dsa/graph_theory/dinic.cpp
@@ -9,7 +9,63 @@ constexpr char nl [[maybe_unused]] = '\n';
 
 // **************************************************************************
 
-void solve(int test_case [[maybe_unused]]) {}
+void solve(int test_case [[maybe_unused]]) {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> cap(n, vector<int>(n));
+	for (int i = 0; i < m; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		u--, v--;
+		cap[u][v] = w;
+	}
+
+	int s, t;
+	cin >> s >> t;
+
+	vector<int> level;
+	vector<bool> vt;
+
+	function<bool()> bfs = [&]() {
+		level.assign(n, -1);
+		deque<int> dq;
+		dq.push_back(s);
+		while (!dq.empty()) {
+			int u = dq.front();
+			dq.pop_front();
+			for (int v = 0; v < n; v++) {
+				if (cap[u][v] && level[v] == -1) {
+					level[v] = level[u] + 1;
+				}
+			}
+		}
+
+		return level[t] != -1;
+	};
+
+	function<int(int, int)> dfs = [&](int u, int pushed) {
+		if (u == t) return pushed;
+		for (int v = 0; v < n; v++) {
+			if (cap[u][v] && level[v] == level[u] + 1) {
+				int tr = dfs(v, min(pushed, cap[u][v]));
+				if (t > 0) {
+					cap[u][v] -= tr;
+					return tr;
+				}
+			}
+		}
+		return 0;
+	};
+
+	ll flow = 0;
+	while (bfs()) {
+		vt.assign(n, false);
+		while (int pushed = dfs(s, INT_MAX)) {
+			flow += pushed;
+		}
+	}
+	cout << flow << nl;
+}
 
 // **************************************************************************
 
