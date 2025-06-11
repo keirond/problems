6de6744fcf337180f6dd3318059a8939aaed4e58
Bao Dang <dang.hbaokody@gmail.com>
2025-06-11 09:45:42 +0700

    up Wed Jun 11 02:45:42 AM UTC 2025

--- dsa/data_structures/link_cut_tree.cpp
+++ dsa/data_structures/link_cut_tree.cpp
@@ -13,110 +13,158 @@ struct Node {
 	Node *left = nullptr, *right = nullptr, *parent = nullptr;
 	int key = 0;
 	int value = 0;
-	bool reversed = false;
 	int sum = 0;
 
-	Node(int key, int value) : key(key), value(value) {}
+	// this signals that the path from x to the root should be reversed
+	bool revert = false;
+	Node(int key, int val) : key(key), value(val) {}
 
-	void pull();
-	void push();
-	void update();
-};
-
-void rotate(Node *x) {
-	Node *p = x->parent;
-	if (!p) return;
-	Node *g = p->parent;
-
-	p->parent = x;
-	x->parent = g;
-	if (p->left == x) {
-		p->left = x->right;
-		if (x->right) x->right->parent = p;
-		x->right = p;
-	} else {
-		p->right = x->left;
-		if (x->left) x->left->parent = p;
-		x->left = p;
+	bool is_root() {
+		return !parent || (parent->left != this && parent->right != this);
 	}
-
-	if (g) {
-		if (g->left == p)
-			g->left = x;
-		else
-			g->right = x;
-	}
-}
-void splay(Node *x) {
-	while (x->parent) {
-		Node *p = x->parent;
-		Node *g = p->parent;
-		if (!g) {
-			rotate(x);
-		} else if ((g->left == p) == (p->left == x)) {
-			rotate(p);
-			rotate(x);
-		} else {
-			rotate(x);
-			rotate(x);
+	void push() {
+		if (revert) {
+			swap(left, right);
+			if (left) left->revert ^= 1;
+			if (right) right->revert ^= 1;
+			revert = false;
 		}
 	}
+
+	void update() {
+		sum = value;
+		if (left) sum += left->sum;
+		if (right) sum += right->sum;
+	}
+
+	void rotate() {
+		Node *p = parent;
+		Node *g = p->parent;
+
+		if (p->left == this) {
+			p->left = right;
+			if (right) right->parent = p;
+			right = p;
+		} else {
+			p->right = left;
+			if (left) left->parent = p;
+			left = p;
+		}
+
+		parent = g;
+		p->parent = this;
+
+		if (g) {
+			if (g->left == p) g->left = this;
+			if (g->right == p) g->right = this;
+		}
+
+		p->update();
+		update();
+	}
+
+	void splay() {
+		while (!is_root()) {
+			Node *p = parent;
+			Node *g = p->parent;
+
+			if (!p->is_root()) g->push();
+			p->push();
+			push();
+
+			if (!p->is_root()) {
+				if ((p->left == this) == (g->left == p)) {
+					p->rotate();
+				} else {
+					rotate();
+				}
+			}
+			rotate();
+		}
+		push();
+	}
+};
+
+// make x the top node of its preferred path, effectively exposing the path fro
+// x to the root
+void access(Node *x) {
+	Node *last = nullptr;
+	for (Node *t = x; t; t = t->parent) {
+		t->splay();
+		t->right = last;
+		t->update();
+		last = t;
+	}
+	x->splay();
 }
 
-void access(Node *u) {
-	Node *last = nullptr;
-	while (u) {
-		splay(u);
-		u->left = last;
-		last = u;
-		u = u->parent;
+// make x the root of entire tree
+void make_root(Node *x) {
+	access(x);
+	x->revert ^= 1;
+}
+
+// connect u -> v, assumming u is a root & u and v are in different trees.
+void link(Node *u, Node *v) {
+	make_root(u);
+	u->parent = v;
+}
+
+// cut the edges between u and its parent;
+void cut(Node *u, Node *v) {
+	make_root(u);
+	access(v);
+	if (v->left == u && !u->right) {
+		v->left = nullptr;
+		u->parent = nullptr;
+		v->update();
 	}
 }
 
-// Tree Queries
-Node *find_root(Node *u);
-int lca(Node *u, Node *v);
-int depth(Node *u);
+// **************************************************************************
 
-// Tree Modifications
-void link(Node *u, Node *v);
-void cut(Node *u, Node *v);
-void make_root(Node *u) {
-	access(u);
-	splay(u);
-	u->rev ^= 1;
-	u->push();
+// return the root of tree containing x.
+Node *find_root(Node *x) {
+	access(x);
+	while (x->left) {
+		x->push();
+		x = x->left;
+	}
+	x->splay();
+	return x;
 }
 
-// Path Operations
+bool connected(Node *u, Node *v) { return find_root(u) == find_root(v); }
+
+//
+int depth(Node *u);
+
+// return the lowest common accestor of u and v
+Node *lca(Node *u, Node *v);
+
+//
 int path_sum(Node *u, Node *v) {
 	make_root(u);
 	access(v);
-	splay(v);
 	return v->sum;
 }
 
-int path_max(Node *u, Node *v);
-void path_update(Node *u, Node *v, int val);
-
-void update(Node *u, int val);
-
-int path_query(Node *u, Node *v) {
-	make_root(u);
-	access(v);
-	return v->subtree_value;
+void update(Node *x, int val) {
+	access(x);
+	x->value = val;
+	x->update();
 }
 
-bool connected(Node *u, Node *v);
+void path_update(Node *u, Node *v, int val);  // require Lazy Propagation
 
 void solve(int test_case [[maybe_unused]]) {
 	int n, q;
 	cin >> n >> q;
 	vector<Node *> tr(n);
 	for (int i = 0; i < n; i++) {
-		int val;
-		cin >> val;
-		tr[i] = new Node(val);
+		int key, val;
+		cin >> key >> val;
+		tr[i] = new Node(key, val);
 	}
 
 	for (int i = 0; i < n - 1; i++) {
@@ -133,7 +181,7 @@ void solve(int test_case [[maybe_unused]]) {
 			int u, v;
 			cin >> u >> v;
 			u--, v--;
-			cout << path_query(tr[u], tr[v]) << nl;
+			cout << path_sum(tr[u], tr[v]) << nl;
 		} else if (op == 2) {
 			int u, v;
 			cin >> u >> v;
