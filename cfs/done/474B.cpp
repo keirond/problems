8ac7af42edd2627afae0f74d006c0b91678515ab
Bao Dang <dang.hbaokody@gmail.com>
2025-06-28 21:36:35 +0700

    up Sat Jun 28 02:36:35 PM UTC 2025

--- cfs/done/474B.cpp
+++ cfs/done/474B.cpp
@@ -5,13 +5,29 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 
 constexpr char nl [[maybe_unused]] = '\n';
 
 // **************************************************************************
 
-void solve(int test_case [[maybe_unused]]) {}
+void solve(int test_case [[maybe_unused]]) {
+	int n;
+	cin >> n;
+	vector<int> nums(n);
+	for (int &d : nums) cin >> d;
+	for (int i = 1; i < n; i++) {
+		nums[i] += nums[i - 1];
+	}
+
+	int m;
+	cin >> m;
+	while (m--) {
+		int d;
+		cin >> d;
+		int it = lower_bound(nums.begin(), nums.end(), d) - nums.begin();
+		cout << it + 1 << nl;
+	}
+}
 
 // **************************************************************************
 
